850d5990394bd9c44819d0942fb6ccc3b9d43547
obanb <ondrej.basista@apitree.cz>
2025-01-24 12:59:52 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {Server} from "node:http";
+import express from 'express';
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+export const startServer = async () => {
+    await new Promise<Server>((resolve) => {
+        const httpServer = app.listen(port, () => {
+            console.log(`[server]: Server is running at http://localhost:${port}`);
+            resolve(httpServer);
+        });
+    });
+
+
+    app.use(express.json({ limit: '1mb' }));
+
+    app.get('/hello', (req, res) => {
+        res.json({"hello":"from api"});
+    })
+};
+
+startServer().catch(console.error);
